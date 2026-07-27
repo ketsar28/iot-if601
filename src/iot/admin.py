43d9465a07008ff94a3541939d703a4e9318ea98
Ketsar Ali <muhammadketsar2@gmail.com>
2026-07-27 17:06:32 +0700

    Cleanup: Final cleanup, only project-8

--- src/iot/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import SensorData
-
-@admin.register(SensorData)
-class SensorDataAdmin(admin.ModelAdmin):
-    list_display=('temperature','humidity','timestamp')
-
