2aaefac883982076fac0a2a6b4780e94b9a283a5
Ketsar Ali <muhammadketsar2@gmail.com>
2026-07-27 17:01:31 +0700

    Cleanup: Maintain only project-8 folder in repository

--- src/iot/utils.py
+++
@@ -1,8 +0,0 @@
-# yourapp/utils.py
-VALID_API_KEYS = {
-    "abc123": "device01",
-    "xyz789": "device02",
-}
-
-def is_valid_apikey(apikey, device_id):
-    return VALID_API_KEYS.get(apikey) == device_id
